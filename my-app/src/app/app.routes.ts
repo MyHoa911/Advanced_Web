8d4cc77d869f1947757afae308724b44a7f31de6
MyHoa <myhoalethi0911@gmail.com>
2026-01-22 08:07:19 +0700

    Update components with fixes

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Listcustomer } from './listcustomer/listcustomer';
 import { Listcustomer2 } from './listcustomer2/listcustomer2';
 import { Listcustomer3 } from './listcustomer3/listcustomer3';
 import { Notfound } from './notfound/notfound';
+import { Listproduct } from './listproduct/listproduct';
 
 export const routes: Routes = [
     {path:"gioi-thieu", component: About},
     {path:"khach-hang-1", component: Listcustomer},
     {path:"khach-hang-2", component: Listcustomer2},
     {path:"khach-hang-3", component: Listcustomer3},
+    {path:"san-pham-1", component: Listproduct},
     {path:"**", component: Notfound}
 ];
